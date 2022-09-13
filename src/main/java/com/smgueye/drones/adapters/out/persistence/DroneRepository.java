8c7b05b66c9b24aca25a30cae10537c89a9daf38
smgueye <gueye.syd.mouhamed@gmail.com>
2022-09-13 00:21:53 +0000

    feat(drone-registration): Drone registration\n
    - Setup hexagonal architecture\n
    - Implement registration use case.\n

--- src/main/java/com/smgueye/drones/adapters/out/persistence/DroneRepository.java
+++
@@ -1,6 +0,0 @@
-package com.smgueye.drones.adapters.out.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DroneRepository extends JpaRepository<DroneJpaEntity, Long> {
-}
