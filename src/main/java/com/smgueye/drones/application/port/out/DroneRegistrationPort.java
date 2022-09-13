b5947d51125c781268498a285e2e0fa4da440695
smgueye <gueye.syd.mouhamed@gmail.com>
2022-09-13 00:21:53 +0000

    feat(drone-registration): Drone registration\n
    - Setup hexagonal architecture\n
    - Implement registration use case.\n

--- src/main/java/com/smgueye/drones/application/port/out/DroneRegistrationPort.java
+++
@@ -1,9 +0,0 @@
-package com.smgueye.drones.application.port.out;
-
-import com.smgueye.drones.application.port.in.EditDroneCommand;
-import com.smgueye.drones.domain.Drone;
-
-public interface DroneRegistrationPort {
-
-  Drone register(EditDroneCommand command);
-}
