cfc58cf305621fe3bad993e75ac3d7da6da0b5a8
smgueye <gueye.syd.mouhamed@gmail.com>
2022-09-20 01:17:09 +0000

    RESOLVE CONFLICT

--- src/main/java/com/smgueye/drones/adapters/out/persistence/DroneRepository.java
+++ src/main/java/com/smgueye/drones/adapters/out/persistence/DroneRepository.java
@@ -7,9 +7,13 @@ import java.util.List;
 
 public interface DroneRepository extends JpaRepository<DroneJpaEntity, Long> {
 <<<<<<< HEAD
+<<<<<<< HEAD
   @Query(value = "SELECT drone FROM DroneJpaEntity drone WHERE drone.state = 1 AND drone.battery >= 25")
 =======
   @Query(value = "SELECT drone FROM DroneJpaEntity drone WHERE drone.state = 1")
 >>>>>>> 5757f89 (feat(delivery-check): Delivery\n)
+=======
+  @Query(value = "SELECT drone FROM DroneJpaEntity drone WHERE drone.state = 1 AND drone.battery >= 25")
+>>>>>>> 1639508 (fix(drone-registration): Registration)
   List<DroneJpaEntity> findAllAvailableDrones();
 }
